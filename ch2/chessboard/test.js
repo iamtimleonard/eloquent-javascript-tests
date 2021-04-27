1630ea172a6328a5a86575b49013b122ea4f806b
iamtimleonard <timleonardcello@gmail.com>
2021-04-27 13:26:21 -0400

    update chessboard tests

--- ch2/chessboard/test.js
+++ ch2/chessboard/test.js
@@ -2,6 +2,18 @@ const chessboard = require("./chessboard");
 
 jest.spyOn(console, "log").mockImplementation();
 
+test("prints to the console once", () => {
+  chessboard();
+  expect(console.log).toBeCalledTimes(1);
+});
+
+test("prints a square", () => {
+  chessboard();
+  let array = console.log.mock.calls[0][0].trimEnd().split(/\n/);
+  expect(array[0].length).toEqual(array.length);
+  expect(array[1].length).toEqual(array[0].length);
+});
+
 test("prints a white square first", () => {
   chessboard();
   expect(console.log.mock.calls[0][0][0]).toEqual(" ");
