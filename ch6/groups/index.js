1a33a02dee7f45ad82d816836cf18a1e0c20fd3a
iamtimleonard <timleonardcello@gmail.com>
2021-05-02 17:52:38 -0400

    ch6 groups

---
+++ ch6/groups/index.js
@@ -0,0 +1,34 @@
+// The standard JavaScript environment provides another data structure called Set. Like an instance of Map, a set holds a collection of values. Unlike Map, it does not associate other values with those—it just tracks which values are part of the set. A value can be part of a set only once—adding it again doesn’t have any effect.
+
+// Write a class called Group (since Set is already taken). Like Set, it has add, delete, and has methods. Its constructor creates an empty group, add adds a value to the group (but only if it isn’t already a member), delete removes its argument from the group (if it was a member), and has returns a Boolean value indicating whether its argument is a member of the group.
+
+// Use the === operator, or something equivalent such as indexOf, to determine whether two values are the same.
+
+// Give the class a static from method that takes an iterable object as argument and creates a group that contains all the values produced by iterating over it.
+
+class Group {
+  constructor() {
+    this.group = [];
+  }
+  has(value) {
+    return this.group.includes(value);
+  }
+  add(value) {
+    if (this.group.indexOf(value) > -1) {
+      return;
+    }
+    this.group.push(value);
+  }
+  delete(value) {
+    this.group.splice(this.group.indexOf(value), 1);
+  }
+  static from([start, finish]) {
+    const group = new Group();
+    for (let i = start; i <= finish; i++) {
+      group.add(i);
+    }
+    return group;
+  }
+}
+
+module.exports = Group;
---
+++ ch6/groups/test.js
@@ -0,0 +1,6 @@
+const Group = require("./index");
+
+describe("Group", () => {
+  //write your solution below
+  //write your solution above
+});
